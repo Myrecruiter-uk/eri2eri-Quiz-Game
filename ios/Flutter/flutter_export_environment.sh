d8dc8055cd52e978473b91e8928ee69ec218c2bb
Abel Tsegaye <abeltsg32@gmail.com>
2021-03-24 00:06:03 +0300

    minor update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Abela\Desktop\New File"
+export "FLUTTER_APPLICATION_PATH=D:\Work\Eri2Eri\eri2eri-Quize-Puzzle-Game"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
